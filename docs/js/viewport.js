55504bcbe6756504693eb7cf32bd131e79cd57e6
skymonsters-Ks <ks_s_monsters@infoseek.jp>
2017-06-17 10:44:43 +0900

    html の viewport テスト

--- docs/js/viewport.js
+++ docs/js/viewport.js
@@ -4,4 +4,4 @@ if (!global_data_env.rate) global_data_env.rate = 1;
 if (!global_data_env.step) global_data_env.step = 5000;
 if (!global_data_env.mem) global_data_env.mem = 32;
 
-document.write('<meta name="viewport" content="width=' + (global_data_env.wx * global_data_env.rate + 120) + ',minimum-scale=1,maximum-scale=1">');
+document.write('<meta name="viewport" content="width=' + (global_data_env.wx * global_data_env.rate + 120) + '">');
